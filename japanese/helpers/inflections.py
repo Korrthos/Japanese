5efb4a9ce8508f711a0105af65d4f4357b1be9de
Ren Tatsumoto <tatsu@autistici.org>
2024-07-29 01:14:39 +0300

    remove optional

--- japanese/helpers/inflections.py
+++ japanese/helpers/inflections.py
@@ -1,14 +1,12 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org> and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-from typing import Optional
-
 from ..mecab_controller.kana_conv import is_kana_char
 from ..mecab_controller.unify_readings import literal_pronunciation as pr
 from ..mecab_controller.unify_readings import replace_handakuten
 
 
-def longest_kana_suffix(word: str) -> Optional[str]:
+def longest_kana_suffix(word: str) -> str:
     last_idx = len(word)
     while (last_idx := last_idx - 1) >= 0:
         if not is_kana_char(word[last_idx]):
