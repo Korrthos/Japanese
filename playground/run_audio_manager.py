be1b41dbfb216c110e0159582d79eb2f47705c01
Ren Tatsumoto <tatsu@autistici.org>
2025-03-03 05:40:37 +0300

    use tmp db path

--- playground/run_audio_manager.py
+++ playground/run_audio_manager.py
@@ -21,28 +21,28 @@ class NoAnkiAudioSourceManagerFactory(AudioSourceManagerFactory):
         )
 
 
-def init_testing_audio_manager() -> NoAnkiAudioSourceManagerFactory:
+def init_testing_audio_manager(db_path: pathlib.Path) -> NoAnkiAudioSourceManagerFactory:
     # Used for testing when Anki isn't running.
-    return NoAnkiAudioSourceManagerFactory(config=NoAnkiConfigView())
+    return NoAnkiAudioSourceManagerFactory(config=NoAnkiConfigView(), db_path=db_path)
 
 
 def main() -> None:
-    factory = init_testing_audio_manager()
-    factory.init_sources()
-    session: AudioSourceManager
-    stats: TotalAudioStats
-
-    with Sqlite3Buddy() as db:
-        session = factory.request_new_session(db)
-        stats = session.total_stats()
-        print(f"{stats.unique_files=}")
-        print(f"{stats.unique_headwords=}")
-        for source_stats in stats.sources:
-            print(source_stats)
-        for file in session.search_word("ひらがな"):
-            print(file)
-        for source in session.audio_sources:
-            print(f"source {source.name} media dir {source.media_dir}")
+    with tmp_sqlite3_db_path() as db_path:
+        factory = init_testing_audio_manager(db_path)
+        factory.init_sources()
+        session: AudioSourceManager
+        stats: TotalAudioStats
+        with Sqlite3Buddy(db_path) as db:
+            session = factory.request_new_session(db)
+            stats = session.total_stats()
+            print(f"{stats.unique_files=}")
+            print(f"{stats.unique_headwords=}")
+            for source_stats in stats.sources:
+                print(source_stats)
+            for file in session.search_word("ひらがな"):
+                print(file)
+            for source in session.audio_sources:
+                print(f"source {source.name} media dir {source.media_dir}")
 
 
 if __name__ == "__main__":
