7731dccb6e3118fe548a97c0e1a9a48d42bfe40e
Ren Tatsumoto <tatsu@autistici.org>
2025-10-02 22:54:52 +0300

    update scripts

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
-readonly root_dir=$(git rev-parse --show-toplevel)
+root_dir=$(git rev-parse --show-toplevel)
+tmpdir=$(mktemp -d)
+output=ajt_japanese.ankiaddon
+readonly root_dir tmpdir output
 
 "$root_dir/japanese/ajt_common/package.sh" \
 	--package "AJT Japanese" \
@@ -10,26 +13,26 @@ readonly root_dir=$(git rev-parse --show-toplevel)
 	--root "japanese" \
 	"$@"
 
-readonly output=ajt_japanese.ankiaddon
-
 if ! [[ -f $output ]]; then
 	echo "Missing file: $output"
 	exit 1
 fi
 
-readonly tmpdir=$(mktemp -d)
+if ! git -C "$root_dir/kanjigrid" diff --quiet --exit-code; then
+	echo "kanjigrid working tree is dirty"
+	exit 1
+fi
+
 # Create the desired directory structure inside tmpdir
-mkdir -p "$tmpdir/kanjigrid"
-mkdir -p "$tmpdir/kanjigrid/data"
+mkdir -p -- "$tmpdir/kanjigrid"
+mkdir -p -- "$tmpdir/kanjigrid/data"
 
 # Copy contents
 cp -a -- "$root_dir/kanjigrid/src"/*.py "$root_dir/kanjigrid/src"/*.json "$tmpdir/kanjigrid/"
 cp -a -- "$root_dir/kanjigrid/data/." "$tmpdir/kanjigrid/data/"
 
 # Add to zip
-
 ( cd -- "$tmpdir" && zip -ur "$root_dir/$output" "kanjigrid" ) # add "kanjigrid" from the PWD.
 
 rm -rf -- "$tmpdir"
-
 echo -e "Added kanjigrid to the archive."
