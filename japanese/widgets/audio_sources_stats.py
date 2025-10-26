3a8fc9a69880cc7b4deed0c3b58133d55d4760e6
Ren Tatsumoto <tatsu@autistici.org>
2025-10-26 03:33:32 +0300

    use class from ajt common

--- japanese/widgets/audio_sources_stats.py
+++ japanese/widgets/audio_sources_stats.py
@@ -3,51 +3,22 @@
 
 import dataclasses
 
-from aqt.qt import *
-
-from ..ajt_common.restore_geom_dialog import AnkiSaveAndRestoreGeomDialog
+from ..ajt_common.stats_table_dialog import StatsDialog
 from ..ajt_common.utils import ui_translate
 from ..audio_manager.basic_types import AudioStats, TotalAudioStats
 
 
-class AudioStatsTable(QTableWidget):
-    def __init__(self, parent=None) -> None:
-        super().__init__(parent)
-        self: QTableWidget
-        self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        self.horizontalHeader().setStretchLastSection(True)
-        self.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
-        self.setColumnCount(len(dataclasses.fields(AudioStats)))
-        self.setHorizontalHeaderLabels([ui_translate(field.name) for field in dataclasses.fields(AudioStats)])
-        self.setStretchAllColumns()
-
-    def setStretchAllColumns(self):
-        header = self.horizontalHeader()
-        for column_number in range(self.columnCount()):
-            header.setSectionResizeMode(column_number, QHeaderView.ResizeMode.Stretch)
-
-
-class AudioStatsDialog(AnkiSaveAndRestoreGeomDialog):
+class AudioStatsDialog(StatsDialog):
     name: str = "ajt__audio_stats_dialog"
-    _table: AudioStatsTable
-    _button_box: QDialogButtonBox
+    win_title: str = "Audio Statistics"
 
     def __init__(self, parent=None) -> None:
-        super().__init__(parent=parent)
-        self: QDialog
-        self.setWindowTitle("Audio Statistics")
+        super().__init__(
+            parent=parent,
+            column_names=[ui_translate(field.name) for field in dataclasses.fields(AudioStats)],
+        )
         self.setMinimumSize(400, 240)
-        self._table = AudioStatsTable()
-        self.setLayout(QVBoxLayout())
-        self._button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
-        self.layout().addWidget(self._table)
-        self.layout().addWidget(self._button_box)
-        qconnect(self._button_box.accepted, self.accept)
-        qconnect(self._button_box.rejected, self.reject)
 
     def load_data(self, stats: TotalAudioStats) -> "AudioStatsDialog":
-        for idx, row in enumerate(stats.sources):
-            self._table.insertRow(idx)
-            for jdx, item in enumerate(dataclasses.astuple(row)):
-                self._table.setItem(idx, jdx, QTableWidgetItem(str(item)))
+        super().load_data([dataclasses.astuple(row) for row in stats.sources])
         return self
