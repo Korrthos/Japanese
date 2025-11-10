90b96580315fb776e5538e9d22daebfc8d76c784
Ren Tatsumoto <tatsu@autistici.org>
2025-11-11 02:22:53 +0300

    parametrize test_pitch_accent_entry

--- tests/test_pitch_accent.py
+++ tests/test_pitch_accent.py
@@ -21,61 +21,114 @@ from japanese.pitch_accents.consts import PITCH_DIR_PATH
 from japanese.pitch_accents.format_accents import format_entry
 
 
-def test_pitch_accent_entry() -> None:
-    entry = PitchAccentEntry.from_formatted(
-        FormattedEntry(
-            raw_headword="楽しい",
-            katakana_reading="たのしい",
-            pitch_number="3",
-            html_notation="たのしい",
-        )
-    )
-
-    token = AccDbParsedToken(
-        word="楽しかった",
-        headword="楽しい",
-        katakana_reading="たのしかった",
-        part_of_speech=PartOfSpeech.i_adjective,
-        inflection_type=Inflection.unknown,
-        headword_accents=(entry,),
-    )
-
-    assert token.describe_pitches() == "たのしい:nakadaka-3"
-
-    entry = PitchAccentEntry.from_formatted(
-        FormattedEntry(
-            raw_headword="納屋",
-            katakana_reading="なや",
-            pitch_number="0,1",
-            html_notation="なや",
-        )
-    )
-
-    token = AccDbParsedToken(
-        word="納屋",
-        headword="納屋",
-        katakana_reading="なや",
-        part_of_speech=PartOfSpeech.noun,
-        inflection_type=Inflection.dictionary_form,
-        headword_accents=(entry,),
-    )
-    assert token.describe_pitches() == "なや:heiban,atamadaka"
-
-    token = AccDbParsedToken(
-        word="粗末",
-        headword="粗末",
-        katakana_reading=None,
-        part_of_speech=PartOfSpeech.unknown,
-        inflection_type=Inflection.dictionary_form,
-        headword_accents=[
-            PitchAccentEntry(
-                katakana_reading="ソマツ",
-                pitches=[PitchParam(type=PitchType.atamadaka, number="1")],
-                katakana_reading_sep="ソマツ",
-            )
-        ],
-    )
-    assert token.describe_pitches() == "ソマツ:atamadaka"
+@pytest.mark.parametrize(
+    "token,  expected_pitches",
+    [
+        (
+            AccDbParsedToken(
+                word="楽しかった",
+                headword="楽しい",
+                katakana_reading="たのしかった",
+                part_of_speech=PartOfSpeech.i_adjective,
+                inflection_type=Inflection.unknown,
+                headword_accents=(
+                    PitchAccentEntry.from_formatted(
+                        FormattedEntry(
+                            raw_headword="楽しい",
+                            katakana_reading="たのしい",
+                            pitch_number="3",
+                            html_notation="たのしい",
+                        ),
+                        PartOfSpeech.i_adjective,
+                    ),
+                ),
+            ),
+            "たのしい:kifuku-3",
+        ),
+        (
+            AccDbParsedToken(
+                word="納屋",
+                headword="納屋",
+                katakana_reading="なや",
+                part_of_speech=PartOfSpeech.noun,
+                inflection_type=Inflection.dictionary_form,
+                headword_accents=(
+                    PitchAccentEntry.from_formatted(
+                        FormattedEntry(
+                            raw_headword="納屋",
+                            katakana_reading="なや",
+                            pitch_number="0,1",
+                            html_notation="なや",
+                        ),
+                        PartOfSpeech.noun,
+                    ),
+                ),
+            ),
+            "なや:heiban,atamadaka",
+        ),
+        (
+            AccDbParsedToken(
+                word="食べる",
+                headword="食べる",
+                katakana_reading="タベル",
+                part_of_speech=PartOfSpeech.verb,
+                inflection_type=Inflection.dictionary_form,
+                headword_accents=(
+                    PitchAccentEntry.from_formatted(
+                        FormattedEntry(
+                            raw_headword="食べる",
+                            katakana_reading="タベル",
+                            pitch_number="1",
+                            html_notation="タベル",
+                        ),
+                        PartOfSpeech.verb,
+                    ),
+                ),
+            ),
+            "タベル:kifuku-1",
+        ),
+        (
+                AccDbParsedToken(
+                    word="食べる",
+                    headword="食べる",
+                    katakana_reading="タベル",
+                    part_of_speech=PartOfSpeech.verb,
+                    inflection_type=Inflection.dictionary_form,
+                    headword_accents=(
+                            PitchAccentEntry.from_formatted(
+                                FormattedEntry(
+                                    raw_headword="食べる",
+                                    katakana_reading="タベル",
+                                    pitch_number="2",
+                                    html_notation="タベル",
+                                ),
+                                PartOfSpeech.verb,
+                            ),
+                    ),
+                ),
+                "タベル:kifuku-2",
+        ),
+        (
+            AccDbParsedToken(
+                word="粗末",
+                headword="粗末",
+                katakana_reading=None,
+                part_of_speech=PartOfSpeech.unknown,
+                inflection_type=Inflection.dictionary_form,
+                headword_accents=[
+                    PitchAccentEntry(
+                        katakana_reading="ソマツ",
+                        pitches=[PitchParam(type=PitchType.atamadaka, number="1", n_moras=3)],
+                        katakana_reading_sep="ソマツ",
+                    )
+                ],
+            ),
+            "ソマツ:atamadaka",
+        ),
+    ],
+)
+def test_pitch_accent_entry(token: AccDbParsedToken, expected_pitches: str) -> None:
+    assert token.describe_pitches() == expected_pitches
 
 
 def test_files_in_dir() -> None:
