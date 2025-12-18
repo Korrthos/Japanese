5d7dc5056cfb505f8bac653e1d1e7d5ff536b439
korrthos <110887596+Korrthos@users.noreply.github.com>
2025-12-18 20:34:22 +0100

    feat: simplest solution is the best solution

--- japanese/helpers/file_ops.py
+++ japanese/helpers/file_ops.py
@@ -52,10 +52,13 @@ def find_config_json() -> pathlib.Path:
 @functools.cache
 def user_files_dir() -> pathlib.Path:
     """Return path to the user files directory."""
-    for parent_dir in walk_parents(__file__):
-        if (dir_path := parent_dir.joinpath("user_files")).is_dir():
-            return dir_path
-    raise RuntimeError("couldn't find user_files directory")
+    dir_path = os.path.join(os.path.expanduser("~"), ".config", "anki", "ajt")
+    os.makedirs(dir_path, exist_ok=True)
+    return dir_path
+    # for parent_dir in walk_parents(__file__):
+    #     if (dir_path := parent_dir.joinpath("user_files")).is_dir():
+    #         return dir_path
+    # raise RuntimeError("couldn't find user_files directory")
 
 
 def open_file(path: str) -> None:
